d6e7356ab659a83b0b9f3a3d510d3c0517c32ae1
draco <ryoi.matz@gmail.com>
2019-05-15 20:56:46 +0900

    make Simplicity

--- src/Parse.hs
+++ src/Parse.hs
@@ -13,45 +13,35 @@ import Control.Arrow
 data Token = EndLine | Ident String | Number Int 
     | Literal String | LiteralOne Char | Symbol String 
     | Comma | ParenOpen | ParenClose | Error String String deriving (Eq, Show)
-
-getToken :: String -> (Maybe Token, String)
-getToken [] = (Nothing, [])
-getToken (x:xs)
-    | isDigit x = make (Number . read) isDigit
-    | isIdentHead x = make Ident isIdentBody
-    | isOperator x = make Symbol isOperator
-    | isBracket x = (Just $ toSymbol [x], xs)
-    | x == '"' = make' Literal ('"' /=)
-    | x == '#' = first (const Nothing) $ span ('\n' /=) xs
-    | x == '\'' = make' toChar ('\'' /=)
-    | isSpace x = (Nothing, xs)
-    | otherwise = (Just $ Error [x] "wrong", xs)
-    where
-        make f g = first (Just . f . (x:)) $ span g xs
-        make' f g = fmap (drop 1) $ first h $ span g xs
-            where h all = Just $ case all of [] -> Error all "no"; _ -> f all
-        [isIdentSymbol, isOperator, isBracket] = map (flip elem) ["_'" , "+-*/\\<>|?=@^$:~`.", "(){}[],"]
-        [isIdentHead, isIdentBody] = map any [[isLetter, ('_' ==)], [isLetter, isDigit, isIdentSymbol]]
-            where any = F.foldr ((<*>) . (<$>) (||)) $ const False
-        toChar all = case all of [x] -> LiteralOne x; [] -> Error all "too short"; (x:xs) -> Error all "too long"
-        toSymbol all = case all of "(" -> ParenOpen; ")" -> ParenClose; "," -> Comma; _ -> Symbol all
-
 tokenize :: String -> [Token]
 tokenize [] = []
-tokenize all = (\(x, xs)-> maybe xs (:xs) x) $ tokenize <$> getToken all
-
-getOpe = flip M.lookup $ M.fromList 
-    [("+", (0, True)), ("-", (0, True)), ("*", (1, True)), ("/", (1, True)), ("^", (2, False))]
-
-precederEq s ParenOpen = False
-precederEq s (Ident _) = True
-precederEq s (Symbol t) = precederEq' $ map getOpe' [s, t]
-    where
-        precederEq' [(aPre, aLeft), (bPre, bLeft)] = bLeft && ((aLeft && aPre <= bPre) || aPre < bPre)
-        getOpe' = fromMaybe (0, True) . getOpe
+tokenize all = (\(x, xs)-> maybe xs (:xs) x) $ tokenize <$> getToken all where
+    getToken :: String -> (Maybe Token, String)
+    getToken [] = (Nothing, [])
+    getToken (x:xs)
+        | isDigit x = make (Number . read) isDigit
+        | isIdentHead x = make Ident isIdentBody
+        | isOperator x = make Symbol isOperator
+        | isBracket x = (Just $ toSymbol [x], xs)
+        | x == '"' = make' Literal ('"' /=)
+        | x == '#' = first (const Nothing) $ span ('\n' /=) xs
+        | x == '\'' = make' toChar ('\'' /=)
+        | isSpace x = (Nothing, xs)
+        | otherwise = (Just $ Error [x] "wrong", xs)
+        where
+            -- (token constructor, tail condition) -> (token, rest string)
+            make f g = first (Just . f . (x:)) $ span g xs
+            make' f g = fmap (drop 1) $ first h $ span g xs
+                where h all = Just $ case all of [] -> Error all "no"; _ -> f all
+            -- Char -> Bool
+            [isIdentSymbol, isOperator, isBracket] = map (flip elem) ["_'" , "+-*/\\<>|?=@^$:~`.", "(){}[],"]
+            [isIdentHead, isIdentBody] = map any [[isLetter, ('_' ==)], [isLetter, isDigit, isIdentSymbol]]
+                where any = F.foldr ((<*>) . (<$>) (||)) $ const False
+            -- String -> Token
+            toChar all = case all of [x] -> LiteralOne x; [] -> Error all "too short"; (x:xs) -> Error all "too long"
+            toSymbol all = case all of "(" -> ParenOpen; ")" -> ParenClose; "," -> Comma; _ -> Symbol all
 
 data Expr = IdentExpr String | FuncExpr Token [Expr] | StringExpr String | NumberExpr Int | Rewrite Rule Expr Expr deriving (Show, Eq)
-
 parseExpr:: State [Token] (Maybe Expr)
 parseExpr = state $ \ts-> parseExpr ts [] [] where--4
     -- (input tokens, operation stack, expression queue) -> (expression, rest token)
@@ -71,9 +61,9 @@ parseExpr = state $ \ts-> parseExpr ts [] [] where--4
             incrementArg = apply (isIdent . fst) (fmap (1+))
             (opes, rest) = incrementArg <$> span ((/= ParenOpen) . fst) s
         Symbol ope  -> case getOpe ope of 
-            Just _ -> parseExpr xs ((x, 2):rest) $ makeExpr opes q where
+            Just _      -> parseExpr xs ((x, 2):rest) $ makeExpr opes q where
                 (opes, rest) = span (precederEq ope . fst) s
-            Nothing -> result 
+            Nothing     -> result 
         where
             result = (Just $ head $ makeExpr s q, all)
             maybeEnd a = if sum (map ((-1+) . snd) s) + 1 == length q then result else a
@@ -82,9 +72,18 @@ parseExpr = state $ \ts-> parseExpr ts [] [] where--4
     makeExpr [] q = q
     makeExpr ((t, n):os) q = makeExpr os $ FuncExpr t args:rest
         where (args, rest) = (reverse $ take n q, drop n q)
-    -- Apply 'f' to a element that satisfy 'cond' for the first time
+    -- apply 'f' to a element that satisfy 'cond' for the first time
     apply cond f all = case b of [] -> all; (x:xs) -> a ++ f x:xs
         where (a, b) = span (not <$> cond) all
+    -- String -> (preceed, left associative)
+    getOpe = flip M.lookup $ M.fromList 
+        [("+", (0, True)), ("-", (0, True)), ("*", (1, True)), ("/", (1, True)), ("^", (2, False))]
+    precederEq:: String -> Token -> Bool
+    precederEq s ParenOpen = False
+    precederEq s (Ident _) = True
+    precederEq s (Symbol t) = precederEq' $ map getOpe' [s, t] where
+        precederEq' [(aPre, aLeft), (bPre, bLeft)] = bLeft && ((aLeft && aPre <= bPre) || aPre < bPre)
+        getOpe' = fromMaybe (0, True) . getOpe
 
 pand:: State [Token] (Maybe (a->b)) -> State [Token] (Maybe a) -> State [Token] (Maybe b)
 pand f a = f >>= maybe (return Nothing) (\f'-> fmap f' <$> a)
@@ -103,9 +102,7 @@ parseToken:: Token -> State [Token] (Maybe Token)
 parseToken x = state $ \case
     [] -> (Nothing, [])
     all@(t:ts) -> if t == x then (Just x, ts) else (Nothing, all)
-
 parseSymbol x = parseToken (Symbol x)
-
 parseIdent = state $ \case
     [] -> (Nothing, [])
     all@(t:ts) -> case t of Ident i-> (Just i, ts); _-> (Nothing, all)
@@ -118,67 +115,80 @@ parseSeparated s p = p >>= \case
             Nothing -> return Nothing
         Nothing -> return $ Just [x']
     Nothing -> return $ Just []
-
 parseCommaSeparated = parseSeparated $ parseToken Comma
 
-parseBlock = return (Just id) `expect` parseSymbol "{" `pand` parseSequence parseStatement `expect` parseSymbol "}"
-
 data VarDecSet = VarDecSet [String] Expr deriving (Show)
+parseVarDecSet:: State [Token] (Maybe VarDecSet)
 parseVarDecSet = return (Just VarDecSet) `pand` parseCommaSeparated parseIdent `expect` parseSymbol ":" `pand` parseExpr
 
 data Statement = StepStm Expr | ImplStm Expr | AssumeStm Expr [Statement] | BlockStm String Expr [Statement] deriving (Show)
+parseStatement:: State [Token] (Maybe Statement)
 parseStatement = parseIdent >>= \case
     Just "step" -> return (Just StepStm) `pand` parseExpr
     Just "impl" -> return (Just ImplStm) `pand` parseExpr
     Just "assume" -> return (Just AssumeStm) `pand` parseExpr `pand` parseBlock
-    _ -> return Nothing
+    _ -> return Nothing 
+    where parseBlock = return (Just id) `expect` parseSymbol "{" `pand` parseSequence parseStatement `expect` parseSymbol "}"
 
-parseVarDecs:: State [Token] (Maybe [(String, Expr)])
+type VarDec = [(String, Expr)]
+parseVarDecs:: State [Token] (Maybe VarDec)
 parseVarDecs = fmap conv (parseCommaSeparated parseVarDecSet) where
     toTuple (VarDecSet ns t) = (ns, repeat t)
     conv = Just . concatMap (uncurry zip) . maybe [] (map toTuple)
 parseParenVarDecs = return (Just id) `expect` parseToken ParenOpen `pand` parseVarDecs `expect` parseToken ParenClose
 
-parseProof = parseSequence parseStatement
-
-type VarDec = [(String, Expr)]
 data Decla = Axiom VarDec Expr | Theorem VarDec Expr [Statement] 
     | Define String VarDec Expr | Predicate String String Expr VarDec Expr deriving (Show)
 parseDecla:: Maybe String -> State [Token] (Maybe Decla)
 parseDecla (Just "axiom") = return (Just Axiom)
     `pand` parseParenVarDecs 
     `expect` parseSymbol "{" `pand` parseExpr `expect` parseSymbol "}"
-
 parseDecla (Just "theorem") = return (Just Theorem) 
     `pand` parseParenVarDecs 
     `expect` parseSymbol "{" 
     `pand` parseExpr 
-    `expect` parseToken (Ident "proof") `expect` parseSymbol ":" `pand` parseProof
+    `expect` parseToken (Ident "proof") `expect` parseSymbol ":" `pand` parseSequence parseStatement
     `expect` parseSymbol "}"
-
 parseDecla (Just "define") = return (Just Define)
     `pand` parseIdent
     `pand` parseParenVarDecs 
     `expect` parseSymbol "{" `pand` parseExpr `expect` parseSymbol "}"
-
 parseDecla (Just "predicate") = return (Just Predicate)
     `pand` parseIdent
     `expect` parseSymbol "[" `pand` parseIdent `expect` parseSymbol ":" `pand` parseExpr `expect` parseSymbol "]"
     `pand` parseParenVarDecs
     `expect` parseSymbol "{" `pand` parseExpr `expect` parseSymbol "}"
-
 parseDecla _ = return Nothing
 
+parseProgram:: State [Token] (Maybe [Decla])
 parseProgram = parseSequence $ parseIdent >>= parseDecla
 
-type Simplicity = [Token]
-
 type Rule = (Expr, Expr)
-type RuleMap = M.Map String [Rule] 
-makeStepRule:: Expr -> RuleMap -> RuleMap -> (RuleMap, RuleMap)
-makeStepRule (FuncExpr (Symbol kind) [a@(FuncExpr (Ident f) _), b]) sm im = case kind of
-    ">>=" -> (M.insert f (a, b) sm, im)
-    "->" -> (sm, M.insert f (a, b) im)
+makeRules:: [Expr] -> ([Rule], [Rule])
+makeRules (x:xs) = if isStep then (r:ms, mi) else (ms, r:mi) where 
+    (isStep, r) = makeRule x
+    (ms, mi) = makeRules xs
+    -- expression -> (is step rule, rule)
+    makeRule:: Expr -> (Bool, Rule)
+    makeRule (FuncExpr (Symbol kind) [a, b]) = case kind of
+        ">>=" -> (True, (a, b))
+        "->" -> (False, (a, b))
+
+type Simplicity = [Token]
+makeSimplicity:: Simplicity -> [Rule] -> Simplicity
+makeSimplicity m [] = m
+makeSimplicity m ((a, b):rs) = makeSimplicity (addSimplicity m a b) rs where
+    addSimplicity:: Simplicity -> Expr -> Expr -> Simplicity
+    addSimplicity m (FuncExpr a _) (FuncExpr b _) = addSimplicity' m a b where
+        addSimplicity' m a b = case (elemIndex a m, elemIndex b m) of
+            (Just a', Just b') -> if a' > b' then error "error" else m
+            (Just a', Nothing) -> insertAt b a' m
+            (Nothing, Just b') -> insertAt a (b'+1) m
+            (Nothing, Nothing) -> b:a:m
+    addSimplicity m _ _ = m
+    insertAt:: a -> Int -> [a] -> [a]
+    insertAt xs 0 as = xs:as
+    insertAt xs i (a:as) = a : insertAt xs (i - 1) as
 
 unify:: Expr -> Expr -> (Bool, M.Map String Expr)
 unify p t = (runState $ unifym p t) M.empty where
@@ -191,18 +201,15 @@ unify p t = (runState $ unifym p t) M.empty where
         unifym' (e:ex) (e':ex') = unifym e e' >>= and (unifym' ex ex')
     unifym (FuncExpr _ _) _ = return False
 
+assign:: M.Map String Expr -> Expr -> Expr
+assign m e@(IdentExpr var) = fromMaybe e $ M.lookup var m
+assign m (FuncExpr f args) = FuncExpr f $ map (assign m) args
+assign m e = e
+
 equals:: (Expr, Expr) -> Bool
 equals (Rewrite _ n _, t) = equals (n, t)
 equals (t, Rewrite _ n _) = equals (t, n)
 equals (a, b) = a == b
-
--- derivate
-type Derivater = (Expr, Expr) -> Maybe Expr
-
-encount:: Eq a => a -> [a] -> (Int, Int)
-encount = encount' (-1, 0) where
-    encount' p _ [] = p
-    encount' (i, n) e (x:xs) = encount' (if n > 0 then i else i + 1, if e == x then n + 1 else n) e xs
 
 applyDiff:: Derivater -> (Expr, Expr) -> Maybe Expr
 applyDiff d pair@(FuncExpr f as, FuncExpr g bs) = if f == g 
@@ -216,15 +223,13 @@ applyDiff d pair@(FuncExpr f as, FuncExpr g bs) = if f == g
         args = zip as bs
         es = fmap equals $ args
         (idx, num) = encount True es
+        -- (element, list) -> (index of the first encountered element, number of encounters)
+        encount:: Eq a => a -> [a] -> (Int, Int)
+        encount = encount' (-1, 0) where
+            encount' p _ [] = p
+            encount' (i, n) e (x:xs) = encount' (if n > 0 then i else i + 1, if e == x then n + 1 else n) e xs
 
-assign:: M.Map String Expr -> Expr -> Expr
-assign m e@(IdentExpr var) = fromMaybe e $ M.lookup var m
-assign m (FuncExpr f args) = FuncExpr f $ map (assign m) args
-assign m e = e
-
-apply:: Rule -> Expr -> Maybe Expr
-apply (a, b) e = if isMatch then Just $ assign map b else Nothing where (isMatch, map) = unify a e
-
+type Derivater = (Expr, Expr) -> Maybe Expr
 derviate:: Rule -> Derivater
 derviate (a, b) (ea, eb) = if isMatch && isMatch' then Just $ assign map eb else Nothing where 
     (isMatch, map) = unify a ea
@@ -233,11 +238,13 @@ derviate (a, b) (ea, eb) = if isMatch && isMatch' then Just $ assign map eb else
 derviateDiff:: Rule -> Derivater
 derviateDiff d = applyDiff $ derviate d
 
-simplify:: RuleMap -> Expr -> Expr
-simplify m e@(FuncExpr h@(Ident f) args) = maybe e' (apply' e) (M.lookup f m) where
+simplify:: [Rule] -> Expr -> Expr
+simplify m e@(FuncExpr h args) = apply' e m where
     e' = FuncExpr h $ map (simplify m) args
     apply' e [] = e
     apply' e (r:rs) = apply' (apply e r) rs
+    apply:: Rule -> Expr -> Maybe Expr
+    apply (a, b) e = if isMatch then Just $ assign map b else Nothing where (isMatch, map) = unify a e
 --addDecra (Axiom vars expr) = 
 
 getTokenTest line = show token ++ ":" ++ rest where (token, rest) = getToken line
