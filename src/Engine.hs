b58d1f1281a94b942bb4b0d1be287a80cffd753a
draco <ryoi.matz@gmail.com>
2019-05-17 04:04:22 +0900

    fix Rewrite bug in simplify

--- src/Engine.hs
+++ src/Engine.hs
@@ -36,8 +36,7 @@ toRuleMap rs = toRuleMap' M.empty $ groupBy equalsHead rs where
     getHead:: [Rule] -> String
     getHead ((FuncExpr h _, _):_) = showHead h
     toRuleMap':: RuleMap -> [[Rule]] -> RuleMap
-    toRuleMap' m [] = m
-    toRuleMap' m (r:rs) = toRuleMap' (M.insert (getHead r) r m) rs
+    toRuleMap' = foldl (\m r-> M.insert (getHead r) r m)
 
 type Simplicity = [String]
 makeSimp:: Simplicity -> [Rule] -> Simplicity
@@ -57,29 +56,40 @@ makeSimp m ((a, b):rs) = makeSimp (addSimp m a b) rs where
     insertAt x i (a:as) = a:insertAt x (i - 1) as
 
 makeRuleMap:: [Expr] -> (RuleMap, RuleMap, Simplicity)
-makeRuleMap xs = (toRuleMap a, toRuleMap b, makeSimp [] a) where (a, b) = makeRules xs
+makeRuleMap xs = (toMap a, toMap b, simp) where 
+    toAppSimp (a, b) = (appSimp simp a, appSimp simp b)
+    toMap = toRuleMap . map toAppSimp
+    (a, b) = makeRules xs
+    simp = makeSimp [] a
+
+extractRewrite:: Expr -> Expr
+extractRewrite (Rewrite _ a _) = extractRewrite a
+extractRewrite (FuncExpr h as) = FuncExpr h $ map extractRewrite as
+extractRewrite x = x
 
 unify:: Expr -> Expr -> Maybe (M.Map String Expr)
 unify p t = if b then Just m else Nothing where
     (b, m) = (runState $ unifym p t) M.empty
-    unifym:: Expr -> Expr -> State (M.Map String Expr) Bool 
-    unifym (IdentExpr var) t = state $ \m-> maybe (True, M.insert var t m) (\x-> (x==t, m)) $ M.lookup var m
+    unifym:: Expr -> Expr -> State (M.Map String Expr) Bool
+    unifym Rewrite{} _ = error "Do not use Rewrite in a rule"
+    unifym e (Rewrite _ a _) = unifym e a
+    unifym (IdentExpr var) t = state $ \m-> maybe (True, M.insert var (extractRewrite t) m) (\x-> (x==t, m)) $ M.lookup var m
     unifym (NumberExpr n) (NumberExpr n') = return $ n == n'
     unifym (NumberExpr _) _ = return False
-    unifym (FuncExpr f ax) (FuncExpr f' ax') = (and $ unifym' ax ax') (f == f') where
+    unifym (FuncExpr f ax) (FuncExpr f' ax') = (and $ unifym' ax ax') (showHead f == showHead f') where
         and f r = if r then f else return False
         unifym' (e:ex) (e':ex') = unifym e e' >>= and (unifym' ex ex')
-    unifym (FuncExpr _ _) _ = return False
+        unifym' [] [] = return True
+        unifym' _ _ = return False
+    unifym FuncExpr{} _ = return False
 
 assign:: M.Map String Expr -> Expr -> Expr
 assign m e@(IdentExpr var) = fromMaybe e $ M.lookup var m
-assign m (FuncExpr f args) = FuncExpr f $ map (assign m) args
+assign m (FuncExpr f as) = FuncExpr f $ map (assign m) as
 assign m e = e
 
 equals:: (Expr, Expr) -> Bool
-equals (Rewrite _ n _, t) = equals (n, t)
-equals (t, Rewrite _ n _) = equals (t, n)
-equals (a, b) = a == b
+equals (a, b) = extractRewrite a == extractRewrite b
 
 applyDiff:: Derivater -> (Expr, Expr) -> Maybe Expr
 applyDiff d pair@(FuncExpr f as, FuncExpr g bs) = if f == g 
@@ -103,36 +113,18 @@ type Derivater = (Expr, Expr) -> Maybe Expr
 derviateDiff:: Rule -> Derivater
 derviateDiff d = applyDiff $ derviate d where
     derviate:: Rule -> Derivater
-    derviate (a, b) (ea, eb) = maybe Nothing f $ unify a ea where
-        f map = maybe Nothing (const $ Just $ assign map eb) $ unify b eb
+    derviate (a, b) (ea, eb) = unify a ea >>= \m-> unify b eb >>= const (Just $ assign m eb)
 
 appSimp :: Simplicity -> Expr -> Expr
 appSimp m (FuncExpr h as) = FuncExpr h' $ map (appSimp m) as where
     h' = case h of PureExprHead t -> ExprHead t $ fromMaybe 0 $ elemIndex (showToken t) m; _-> h
 appSimp _ e = e
 
-lookupHeads:: Expr -> [ExprHead]
-lookupHeads (FuncExpr h as) = h:concatMap lookupHeads as
-lookupHeads _ = []
-
-apply:: [Rule] -> Expr -> Maybe Expr
-apply (r@(a, b):rs) e = maybe (apply rs e) (\m-> Just $ Rewrite r (assign m b) e) (unify a e)
-apply [] e = Nothing
-
 applyArgs:: (Expr -> Maybe Expr) -> [Expr] -> Maybe [Expr]
-applyArgs f = applyArgs' [] where
+applyArgs f xs = applyArgs' xs [] where
     applyArgs':: [Expr] -> [Expr] -> Maybe [Expr]
     applyArgs' [] _ = Nothing
     applyArgs' (a:as) as' = maybe (applyArgs' as (a:as')) (\x-> Just $ as ++ x:as') (f a)
-
-applyAtSimp:: [Rule] -> Int -> Expr -> Maybe Expr
-applyAtSimp rs s e@(FuncExpr h@(ExprHead t s') as) = 
-    if s == s' then apply rs e <|> e' else e' where
-        e' = applyArgs (applyAtSimp rs s) as >>= Just . FuncExpr h
-
-applyByHeadList:: RuleMap -> [ExprHead] -> Expr -> Maybe Expr
-applyByHeadList _ [] _ = Nothing
-applyByHeadList m (ExprHead f s:xs) e = (M.lookup (showToken f) m >>= \x-> applyAtSimp x s e) <|> applyByHeadList m xs e
 
 simplify:: RuleMap -> Expr -> Expr
 simplify m e = maybe e (simplify m) $ step m e where
@@ -141,16 +133,46 @@ simplify m e = maybe e (simplify m) $ step m e where
         simpCompare (ExprHead _ a) (ExprHead _ b) = a `compare` b
         heads = sortBy simpCompare $ lookupHeads e
 
-nextStep:: Expr -> Maybe Expr
-nextStep (Rewrite _ a b) = Just a
-nextStep (FuncExpr h as) = applyArgs nextStep as >>= Just . FuncExpr h
-nextStep _ = Nothing
+    lookupHeads:: Expr -> [ExprHead]
+    lookupHeads x = lookupHeads' $ extractRewrite x where
+        lookupHeads' (FuncExpr h as) = h:concatMap lookupHeads' as
+        lookupHeads' _ = []
+
+    apply:: [Rule] -> Expr -> Maybe Expr
+    apply (r@(a, b):rs) e = maybe (apply rs e) (\m-> Just $ Rewrite r (assign m b) e) (unify a e)
+    apply [] e = Nothing
+    
+    applyAtSimp:: [Rule] -> Int -> Expr -> Maybe Expr
+    applyAtSimp rs s (Rewrite r a b) = applyAtSimp rs s a >>= \x-> Just $ Rewrite r x b
+    applyAtSimp rs s e@(FuncExpr h@(ExprHead t s') as) = 
+        if s == s' then apply rs e <|> e' else e' where
+            e' = applyArgs (applyAtSimp rs s) as >>= Just . FuncExpr h
+    applyAtSimp _ _ _ = Nothing
+    
+    applyByHeadList:: RuleMap -> [ExprHead] -> Expr -> Maybe Expr
+    applyByHeadList _ [] _ = Nothing
+    applyByHeadList m (ExprHead f s:xs) e = (M.lookup (showToken f) m >>= \x-> applyAtSimp x s e) <|> applyByHeadList m xs e
+
 
 showSteps:: Expr -> [Expr]
-showSteps = showSteps' [] where
+showSteps x = showSteps' [x] x where
     showSteps':: [Expr] -> Expr -> [Expr]
     showSteps' p e = maybe p (\x-> showSteps' (x:p) x) $ nextStep e
 
+    nextStep:: Expr -> Maybe Expr
+    nextStep (Rewrite _ a b) = Just a
+    nextStep (FuncExpr h as) = applyArgs nextStep as >>= Just . FuncExpr h
+    nextStep _ = Nothing
+
+showExpr:: Expr -> String
+showExpr (Rewrite _ a b) = showExpr b
+showExpr (StringExpr s) = "\"" ++ s ++ "\"" 
+showExpr (IdentExpr x) = x
+showExpr (NumberExpr n) = show n
+showExpr (FuncExpr h as) = if isAlpha (head f) || length as /= 2 
+    then f ++ "(" ++ intercalate "," (map showExpr as) ++ ")"
+    else let [a, b] = as in showExpr a ++ f ++ showExpr b where f = showHead h
+ 
 program = "axiom () { a+0 >>= a }"
 declas = runState parseProgram . tokenize $ program
 
--- src/Test.hs
+++ src/Test.hs
@@ -13,22 +13,26 @@ tokenizeTest line = intercalate "," $ map show $ tokenize line
 parserTest x = show . runState x . tokenize
 
 simplifyTest:: String -> String
-simplifyTest str = "result:" ++ show out ++ "\n"
-        ++ "expr':" ++ show expr' ++ "\n"
-        ++ "expr'':"  ++ show expr'' ++ "\n"
-        ++ "expr:" ++ show expr ++ "\n" 
+simplifyTest str = "out:" ++ out ++ "\n"
+        -- ++ "out':" ++ out' ++ "\n"
+        -- ++ "simp:" ++ show simp ++ "\n"
+        -- ++ "expr':" ++ show expr' ++ "\n"
+        -- ++ "expr'':"  ++ show expr'' ++ "\n"
+        -- ++ "expr:" ++ show expr ++ "\n" 
         ++ "simpList:" ++ show simpList ++ "\n"
         ++ "stepRule:" ++ show stepRule ++ "\n"
         ++ "implRule:" ++ show implRule ++ "\n"
-        ++ "declas:" ++ show declas ++ "\n" 
-        ++ "props:" ++ show declas ++ "\n"
-        ++ "rules:" ++ show (makeRules props) ++ "\n" where
+        -- ++ "declas:" ++ show declas ++ "\n" 
+        -- ++ "props:" ++ show declas ++ "\n"
+        -- ++ "rules:" ++ show (makeRules props) ++ "\n" 
+        where
     expr' = evalState parseExpr (tokenize str)
     expr'' = fromMaybe (error "wrong expr") expr'
     expr = appSimp simpList expr''
     simp = simplify stepRule expr
-    steps = showSteps simp
-    out = intercalate "\n" (map show steps)
+    steps = reverse $ showSteps simp
+    out = intercalate "\n=" (map showExpr steps)
+    out' = intercalate "\n=" (map show steps)
 
 unifyTest:: String -> String
 unifyTest str = out where
@@ -36,6 +40,8 @@ unifyTest str = out where
     [a, b] = exprs
     out = show $ unify a b
 
+a = unify (IdentExpr "a") (Rewrite (IdentExpr "", IdentExpr "") (IdentExpr "x") (IdentExpr "y"))
+
 test x = forever $ getLine >>= (putStrLn . x)
 -- testFunc = test $ parserTest parseVarDecs 
-testFunc = test $ unifyTest
+testFunc = test simplifyTest
